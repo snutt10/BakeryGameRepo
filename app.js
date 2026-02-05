efd11b8cb1086aad2ebde17ea59f757361734e70
snutt10 <snutt@student.neumont.edu>
2026-02-04 19:51:50 -0700

    adding basic layout

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ console.log("Bakery Game WepPage");
 const express = require("express");
 const { name } = require("ejs");
 const app = express();
-const port = 3000;
+const port = 8080;
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
+app.get("/", (req, res) => {
+    res.render("index", { name: "Bakery Game" });
+});
 
 
 app.listen(port, () => {
